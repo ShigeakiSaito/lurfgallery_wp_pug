59c7af005e140afd9c6794c6491c2c494cfc8d8a
M-NAKAYAMA\m.nakayama <m.nakayama@kannart.co.jp>
2026-02-05 13:21:10 +0900

    MV調整

--- assets/js/top.js
+++ assets/js/top.js
@@ -1,54 +1,60 @@
 window.addEventListener('load', () => {
   // topMvSwiper
+  const mvContainer = document.querySelector('.top__mv');
   const progressWrapper = document.querySelector('.mv-progressbars');
+  const controllerWrapper = document.querySelector('.swiper-controller-wrapper');
 
+  // 複製スライドを除いた実質的な枚数
   const slides = document.querySelectorAll(
     '#topMvSwiper .swiper-slide:not(.swiper-slide-duplicate)'
   );
 
-  progressWrapper.innerHTML = '';
+  // --- ここから枚数判定 ---
+  if (slides.length <= 1) {
+    // 1枚以下の時の処理
+    if (controllerWrapper) controllerWrapper.style.display = 'none';
+    if (mvContainer) mvContainer.classList.add('is-single');
+    
+  } else {
+    // 2枚以上ある時のみ、今までの処理を実行
+    progressWrapper.innerHTML = '';
+    slides.forEach(() => {
+      const bar = document.createElement('div');
+      bar.className = 'mv-progressbar';
+      const span = document.createElement('span');
+      bar.appendChild(span);
+      progressWrapper.appendChild(bar);
+    });
 
-  slides.forEach(() => {
-    const bar = document.createElement('div');
-    bar.className = 'mv-progressbar';
+    const progressBars = progressWrapper.querySelectorAll('.mv-progressbar span');
 
-    const span = document.createElement('span');
-    bar.appendChild(span);
-
-    progressWrapper.appendChild(bar);
-  });
-
-  const progressBars = progressWrapper.querySelectorAll('.mv-progressbar span');
-
-  const topMvSwiper = new Swiper('#topMvSwiper', {
-    loop: true,
-    speed: 800,
-    autoplay: {
-      delay: 4000,
-      disableOnInteraction: false,
-    },
-
-    navigation: {
-      nextEl: '.mv-button-next',
-      prevEl: '.mv-button-prev',
-    },
-
-    on: {
-      autoplayTimeLeft(swiper, time, progress) {
-        const active = swiper.realIndex;
-
-        progressBars.forEach((bar, i) => {
-          if (i < active) {
-            bar.style.width = '100%';
-          } else if (i === active) {
-            bar.style.width = `${(1 - progress) * 100}%`;
-          } else {
-            bar.style.width = '0%';
-          }
-        });
+    const topMvSwiper = new Swiper('#topMvSwiper', {
+      loop: true,
+      speed: 800,
+      autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
       },
-    },
-  });
+      navigation: {
+        nextEl: '.mv-button-next',
+        prevEl: '.mv-button-prev',
+      },
+      on: {
+        autoplayTimeLeft(swiper, time, progress) {
+          const active = swiper.realIndex;
+          progressBars.forEach((bar, i) => {
+            if (i < active) {
+              bar.style.width = '100%';
+            } else if (i === active) {
+              bar.style.width = `${(1 - progress) * 100}%`;
+            } else {
+              bar.style.width = '0%';
+            }
+          });
+        },
+      },
+    });
+  }
 
   // news swiper 
   const topNewsSwiper = new Swiper('#topNewsSwiper', {
