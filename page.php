0fca1e5a498f19bfa437508335f3006e7a4808f8
M-NAKAYAMA\m.nakayama <m.nakayama@kannart.co.jp>
2026-03-11 09:54:19 +0900

    FAQ

--- page.php
+++ page.php
@@ -7,7 +7,9 @@ get_header();
   <?php while ( have_posts() ) : the_post(); ?>
     <main class="page">
       <div class="page__inner">
-        <h1 class="page__title"><?php the_title(); ?></h1>
+        <?php if ( ! is_page('about') ) : ?>
+          <h1 class="page__title"><?php the_title(); ?></h1>
+        <?php endif; ?>
         <div class="page__editable">
           <?php the_content(); ?>
         </div>
