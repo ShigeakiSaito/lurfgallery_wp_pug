cc644a96904f3c6389bde35ac3222c83e3cb01f8
hrshishym <7964648+hrshishym@users.noreply.github.com>
2026-03-14 02:22:43 +0900

    News詳細：メインビジュアル画像をカスタムフィールド化

--- single-post.php
+++ single-post.php
@@ -17,9 +17,24 @@
 					</div>
 					<h2 class="news-detail__title"><?php the_title(); ?></h2>
 				</div>
-				<?php if (has_post_thumbnail()) : ?>
+				<?php
+				$main_visual = get_field('main_visual');
+				$has_mv = !empty($main_visual['pc']) || !empty($main_visual['sp']);
+				?>
+				<?php if ($has_mv || has_post_thumbnail()) : ?>
 				<figure class="news-detail__mv">
+					<?php if (!empty($main_visual['pc']) && !empty($main_visual['sp'])) : ?>
+					<picture>
+						<source media="(max-width: 767px)" srcset="<?php echo esc_url($main_visual['sp']['url']); ?>">
+						<img src="<?php echo esc_url($main_visual['pc']['url']); ?>" alt="<?php echo esc_attr($main_visual['pc']['alt']); ?>">
+					</picture>
+					<?php elseif (!empty($main_visual['pc'])) : ?>
+					<img src="<?php echo esc_url($main_visual['pc']['url']); ?>" alt="<?php echo esc_attr($main_visual['pc']['alt']); ?>">
+					<?php elseif (!empty($main_visual['sp'])) : ?>
+					<img src="<?php echo esc_url($main_visual['sp']['url']); ?>" alt="<?php echo esc_attr($main_visual['sp']['alt']); ?>">
+					<?php elseif (has_post_thumbnail()) : ?>
 					<img src="<?php echo esc_url(get_the_post_thumbnail_url(get_the_ID(), 'large')); ?>" alt="<?php the_title_attribute(); ?>">
+					<?php endif; ?>
 					<?php $caption = get_field('caption'); ?>
 					<?php if ($caption) : ?>
 					<figcaption><?php echo esc_html($caption); ?></figcaption>
